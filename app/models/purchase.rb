4c2fc7d50715b169d960bb1962395830c170d5ee
Sharynne Azhar <sharynneazhar@gmail.com>
2017-01-04 16:28:30 -0600

    fix typo

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,7 +6,7 @@ class Purchase < ActiveRecord::Base
   end
 
   def email_purchaser
-    PurchaseMailer.purchase_receipt(self) .deliver
+    PurchaseMailer.purchase_receipt(self).deliver
   end
 
 end
