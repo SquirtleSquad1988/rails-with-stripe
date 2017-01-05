9c2c397d9368078c4e7288519f88416cc48381a7
Sharynne Azhar <sharynneazhar@gmail.com>
2017-01-05 15:09:32 -0600

    added first product to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin2@example.com', password: 'password', password_confirmation: 'password')
+
+signature = Product.create(
+  title: "Signature",
+  subtitle: "Sharynne Azhar's signature",
+  image_name: "Signature.png",
+  price: "4.99",
+  sku: "signatureOne",
+  download_url: "https://s3.amazonaws.com/rails-with-stripe/Signature.png",
+  details: "A really nice signature",
+  description: %{<p>It's a signature</P>}
+)
